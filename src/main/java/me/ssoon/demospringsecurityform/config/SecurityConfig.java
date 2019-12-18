1cb834d5ad831de10691d745dd5684d3afc387a8
soohoonlee <jjinimania@gmail.com>
2019-12-18 17:45:48 +0900

    Add InMemory Users

--- src/main/java/me/ssoon/demospringsecurityform/config/SecurityConfig.java
+++ src/main/java/me/ssoon/demospringsecurityform/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package me.ssoon.demospringsecurityform.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,5 +20,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
           .formLogin()
         .and()
           .httpBasic();
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication()
+          .withUser("soohoon").password("{noop}123").roles("USER")
+        .and()
+          .withUser("admin").password("{noop}!@#").roles("ADMIN");
   }
 }
