d8d0e05de20aac5c8187eaa3d6406d9599a77210
soohoonlee <jjinimania@gmail.com>
2020-01-06 16:15:54 +0900

    Integrate JPA

--- src/main/java/me/ssoon/demospringsecurityform/config/SecurityConfig.java
+++ src/main/java/me/ssoon/demospringsecurityform/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package me.ssoon.demospringsecurityform.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,20 +12,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
-          .mvcMatchers("/", "/info").permitAll()
+          .mvcMatchers("/", "/info", "/account/**").permitAll()
           .mvcMatchers("/admin").hasRole("ADMIN")
           .anyRequest().authenticated()
         .and()
           .formLogin()
         .and()
           .httpBasic();
-  }
-
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.inMemoryAuthentication()
-          .withUser("soohoon").password("{noop}123").roles("USER")
-        .and()
-          .withUser("admin").password("{noop}!@#").roles("ADMIN");
   }
 }
